5d68f5b7616b8433a2312694f44ab95954deb455
sdingcat <luankang11111@126.com>
2018-09-12 19:08:57 +0800

    first homework1

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tempchar = "";
+            int a, b, mul;
+            System.Console.Write("Please input an int:");
+            tempchar = Console.ReadLine();
+            a = Int32.Parse(tempchar);
+            System.Console.Write("Please input an int:");
+            tempchar = Console.ReadLine();
+            b = Int32.Parse(tempchar);
+            mul = a * b;
+            Console.WriteLine("Multiply is :" + mul);
+        }
+    }
+}
---
+++ Homework1/Homework1_2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            string s2 = textBox2.Text;
+            double d1 = double.Parse(s1);
+            double d2 = double.Parse(s2);
+            double res = d1 * d2;
+            label2.Text = "Resule:" + res;
+        }
+    }
+}
