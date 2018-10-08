a6a764975d00f00d47c0fab23f707df6381502cb
sdingcat <luankang11111@126.com>
2018-10-08 21:02:27 +0800

    Homework4

---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    //声明参数类型
+    public class ClockEventArgs:EventArgs
+    {
+        public bool alert;
+    }
+
+    //声明委托类型
+    public delegate void ClockEventHandler(object sender, ClockEventArgs e);
+
+    //定义事件源
+    public class Theclock
+    {
+        //设置当前时间
+        public int nowHour, nowMinute, nowSecond;
+        public void GetNowTime()
+        {
+            nowHour = DateTime.Now.Hour;
+            nowMinute = DateTime.Now.Minute;
+            nowSecond = DateTime.Now.Second;
+        }
+
+        //设置闹钟
+        public int aimHour=-1, aimMinute=-1, aimSecond=-1;
+        public void SetAlertTime()
+        {
+            Console.WriteLine("依次输入时， 分， 秒（每输入一个数字敲一次回车）：");
+            aimHour = int.Parse(Console.ReadLine());
+            aimMinute = int.Parse(Console.ReadLine());
+            aimSecond = int.Parse(Console.ReadLine());
+        }
+
+        //对比是否到达时间
+        public bool IsTime()
+        {
+            if(nowHour == aimHour&&nowMinute == aimMinute&&nowSecond == aimSecond)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //声明事件
+        public event ClockEventHandler ClockSound;
+
+        public void TheClock()
+        {
+            while(true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                GetNowTime();
+                if(IsTime())
+                {
+                    ClockEventArgs args = new ClockEventArgs();
+                    args.alert = true;
+                    ClockSound(this, args);
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void ShowAlert(object sender,ClockEventArgs e)
+        {
+            Console.WriteLine("时间到"+ e.alert);
+        }
+
+        static void Main(string[] args)
+        {
+            var clock = new Theclock();
+            clock.SetAlertTime();
+            //注册事件
+            clock.ClockSound += ShowAlert;
+            clock.TheClock();
+        }
+    }
+}
---
+++ Homework4/Homework4_2/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*本次作业采用链表存储*/
+
+namespace Homework4_2
+{
+    //订单明细
+    class OrderDetails
+    {
+        public string name;
+        public int number;
+        public double price;
+        public double allprise;
+        public OrderDetails next;
+        public OrderDetails(string name, int number, double price)
+        {
+            this.name = name;
+            this.number = number;
+            this.price = price;
+            allprise = this.number * this.price;
+            next = null;
+        }
+    }
+
+    //订单
+    class Order
+    {
+        public int orderNumber;
+        public OrderDetails head;
+        public Order next;
+        //初始化
+        public Order(int orderNumber)
+        {
+            this.orderNumber = orderNumber;
+            head = null;
+            next = null;
+        }
+        //添加明细
+        public void addOrderDetails(OrderDetails head)
+        {
+            Console.WriteLine("依次输入商品名称，购买数量，单价：");
+            string name = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
+            double prise = double.Parse(Console.ReadLine());
+            OrderDetails newOrderDetails = new OrderDetails(name,number,prise);
+            newOrderDetails.next = head.next;
+            head.next = newOrderDetails;
+        }
+        //按商品名删除明细
+        public void delOrderDetails(OrderDetails head)
+        {
+            Console.WriteLine("输入要删除的商品名称：");
+            string aimName = Console.ReadLine();
+            OrderDetails pre = head, p = head.next;
+            while (p != null)
+            {
+                if(string.Equals(aimName,p.name))
+                {
+                    pre.next = p.next;
+                    p = pre.next;
+                }
+                else
+                {
+                    pre = pre.next;
+                    p = p.next;
+                }
+            }
+        }
+        //输出所有明细
+        void DisOrderDetails(OrderDetails head)
+        {
+            OrderDetails p = head.next;
+            while (p != null)
+            {
+                Console.WriteLine(p.name + "\t" + p.number + "\t" + p.price + "\t" + p.allprise);
+                p = p.next;
+            }
+            Console.WriteLine("\n");
+        }
+        //删除所有明细
+        public void DesOrderDetails(OrderDetails head)
+        {
+            head = null;
+        }
+    }
+
+    //订单服务
+    class OrderService
+    {
+        public Order first;
+        //初始化
+        public OrderService()
+        {
+            first = null;
+        }
+        //添加订单,返回当前订单
+        public Order addOrder(Order first)
+        {
+            Console.WriteLine("输入订单号");
+            int number = int.Parse(Console.ReadLine());
+            Order newOrder = new Order(number);
+            newOrder.next = first.next;
+            first.next = newOrder;
+            return newOrder;
+        }
+        //按订单号删除订单
+        public void delOrder(Order first)
+        {
+            Console.WriteLine("输入要删除的订单号：");
+            int aimNumber =int.Parse(Console.ReadLine());
+            Order pre = first, p = first.next;
+            bool isFind = false;
+            while (p != null)
+            {
+                if (int.Equals(aimNumber, p.orderNumber))
+                {
+                    pre.next = p.next;
+                    p = pre.next;
+                    isFind = true;
+                }
+                else
+                {
+                    pre = pre.next;
+                    p = p.next;
+                }
+            }
+            if(!isFind)
+            {
+                Console.WriteLine("无该订单");
+            }
+        }
+        //输出所有订单(无明细)
+        void DisOrderWith(Order first)
+        {
+            if(first.next==null)
+            {
+                Console.WriteLine("没有订单");
+            }
+            else
+            {
+                Order p = first.next;
+                while (p != null)
+                {
+                    Console.WriteLine(p.orderNumber);
+                    p = p.next;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+        //输出所有订单(有明细)
+        void DisOrderWithout(Order first)
+        {
+            if (first.next == null)
+            {
+                Console.WriteLine("没有订单");
+            }
+            else
+            {
+                Order p = first.next;
+                while (p != null)
+                {
+                    Console.WriteLine(p.orderNumber);
+                    p.DesOrderDetails(p.head);
+                    p = p.next;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+        //删除所有订单
+        public void DesOrder(Order first)
+        {
+            first = null;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderService newOrderService = new OrderService();
+            int again = 0;
+            do
+            {
+                Console.WriteLine("添加订单按1，查找订单按2，输出订单按3，删除订单按4：");
+                int myChoice = int.Parse(Console.ReadLine());
+                switch (myChoice)
+                {
+                    case 1:
+                        Order newOrder = newOrderService.addOrder(newOrderService.first);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("非法输入");
+                        break;
+                }
+                Console.WriteLine("是否继续输入：1-继续，其他任意键-退出");
+                again = int.Parse(Console.ReadLine());
+            } while (again==1);
+            
+        }
+    }
+}
