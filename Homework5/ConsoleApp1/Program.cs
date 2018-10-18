bd9f207c14aef16360adf3181ad8fe97e9748c1d
sdingcat <luankang11111@126.com>
2018-10-18 18:57:44 +0800

    Homework5

---
+++ Homework5/ConsoleApp1/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //订单明细
+    class OrderDetails
+    {
+        public string name;
+        public int number;
+        public double price;
+        public double allPrice;
+    }
+
+    //订单
+    class Order
+    {
+        public int orderNumber=0;
+        public double allPrise=0;
+        public List<OrderDetails> orderDetails = new List<OrderDetails>();
+        //新建订单时需要输入订单号
+        public Order(int orderNumber)
+        {
+            this.orderNumber = orderNumber;
+        }
+        //添加明细
+        public void addOrderDetails(string name, int number, double prise)
+        {
+            OrderDetails newOrderDetails = new OrderDetails();
+            newOrderDetails.name = name;
+            newOrderDetails.number = number;
+            newOrderDetails.price = prise;
+            newOrderDetails.allPrice = number * prise;
+            allPrise += newOrderDetails.allPrice;
+            orderDetails.Add(newOrderDetails);
+        }
+        //按商品名删除明细
+        public void delOrderDetails(string aimName)
+        {
+            var aimOrderList = from aimOrder in orderDetails where aimOrder.name.Equals(aimName) orderby aimOrder.name descending select aimOrder;
+            foreach(var aimOrder in aimOrderList)
+            {
+                allPrise -= aimOrder.allPrice;
+                orderDetails.Remove(aimOrder);
+            }
+        }
+        //输出所有明细
+        public void DisOrderDetails()
+        {
+            foreach (var aimOrder in orderDetails)
+            {
+                Console.WriteLine(aimOrder);
+            }
+        }
+        //删除所有明细
+        public void DesOrderDetails()
+        {
+            foreach (var aimOrder in orderDetails)
+            {
+                allPrise -= aimOrder.allPrice;
+                orderDetails.Remove(aimOrder);
+            }
+        }
+    }
+
+    //订单服务
+    class OrderService
+    {
+        public List<Order> orders = new List<Order>();
+        //添加订单
+        public void addOrder(Order newOrder)
+        {
+            orders.Add(newOrder);
+        }
+        //按金额删除订单
+        public void delOrderByPrise(int aimPrise)
+        {
+            var aimOrderList = from aimOrder in orders where aimOrder.allPrise>aimPrise orderby aimOrder.orderNumber descending select aimOrder;
+            foreach (var aimOrder in aimOrderList)
+            {
+                orders.Remove(aimOrder);
+            }
+        }
+        //按订单号删除订单
+        public void delOrderByNumber(int aimNumber)
+        {
+            var aimOrderList = from aimOrder in orders where aimOrder.orderNumber.Equals(aimNumber) orderby aimOrder.orderNumber descending select aimOrder;
+            foreach (var aimOrder in aimOrderList)
+            {
+                orders.Remove(aimOrder);
+            }
+        }
+        //输出所有订单(有明细)
+        public void DisOrderWithout()
+        {
+            foreach (var aimOrder in orders)
+            {
+                Console.WriteLine(aimOrder);
+            }
+        }
+        //删除所有订单
+        public void DesOrder()
+        {
+            foreach (var aimOrder in orders)
+            {
+                aimOrder.DesOrderDetails();
+                orders.Remove(aimOrder);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Homework5/Homework5/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           if(graphics==null)
+            {
+                graphics = this.CreateGraphics();
+            }
+            drawCayleyTree(10, 200, 310, 100,100, -Math.PI / 2);
+        }
+
+        private Graphics graphics;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.6;
+
+        void drawCayleyTree(int n, double x0, double y0, double lengl, double leng2, double th)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+
+            double x1 = x0 + lengl * Math.Cos(th);
+            double y1 = y0 + lengl * Math.Sin(th);
+            double x2 = x0 + leng2 * Math.Cos(th);
+            double y2 = y0 + leng2 * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1, x2, y2);
+
+            th1 = int.Parse(textBox1.Text) * Math.PI / 180;
+            th2 = int.Parse(textBox2.Text) * Math.PI / 180;
+            
+            drawCayleyTree(n - 1, x1, y1, per1 * lengl, per2 * leng2, th + th1);
+            drawCayleyTree(n - 1, x2, y2, per1 * lengl, per2 * leng2, th - th2);
+        }
+
+        void drawLine(double x0,double y0,double x1,double y1,double x2, double y2)
+        {
+            graphics.DrawLine(Pens.Blue, (int)x0, (int)y0, (int)x1, (int)y1);
+            graphics.DrawLine(Pens.Blue, (int)x0, (int)y0, (int)x2, (int)y2);
+        }
+    }
+}
